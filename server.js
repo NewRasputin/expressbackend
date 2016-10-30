882bd44f9d34097577c88117429ae048af73be9c
NewRasputin <newrasputin1@gmail.com>
2016-10-30 15:58:04 -0700

    Added body-parser

--- server.js
+++ server.js
@@ -1,6 +1,11 @@
 var express = require('express')
+var bodyParser = require('body-parser')
 var app = express()
 var router = express.Router()
+
+app.use(bodyParser.urlencoded({ extended: true }))
+
+app.use(bodyParser.json())
 
 var port = process.env.PORT || 5000
 
